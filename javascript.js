f58ae5ae18dbf128a8197c4f263b507ec5852420
Sochilly <165228069+SochillyInept@users.noreply.github.com>
2024-06-19 18:07:46 -0400

    Fixed issue in getComputerChoice returning 0

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@ console.log("Hello World!")
 
 function getComputerChoice(){
     let choiceCalc = function () { return Math.floor(Math.random() * 10)} //Getting a random decimal from 0 - 1 but not 1 and multiply it by 10. Round down to turn it into an integer with Math.Floor.  
-    let choice;
-    (choiceCalc() < 1) ? choiceCalc() : // If choiceCalc is 0 run it again that way we calculate from 1-9 not 0-9. 1-3 is Rock, 4-6 is Paper, 7-9 is Scissors. 3/9
-    (choiceCalc() < 4) ? choice = "rock" :
-    (choiceCalc() > 6) ? choice = "paper" : choice = "scissors";
+    let choice = choiceCalc();
+    (choice < 1) ? choice = getComputerChoice() : // If choiceCalc is 0 run it again that way we calculate from 1-9 not 0-9. 1-3 is Rock, 4-6 is Paper, 7-9 is Scissors. 3/9
+    (choice < 4) ? choice = "rock" :
+    (choice > 6) ? choice = "paper" : choice = "scissors";
     return choice;
 }
 
@@ -35,19 +35,27 @@ function getHumanChoice(){
     else {alert("Hmmm... That was not a valid choice.")}
 }
 
+//Score variables
+let humanScore = 0;
+let computerScore = 0;
+
 
 //  Compare getComputerChoice and getHumanChoice and identify winner based on if the player wins or not
 
 function playerWinsif(){
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
     if (computerChoice == "rock" && humanChoice == "paper"){
-        alert("Player wins!")
+        console.log(`Player wins! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`)
+        humanScore += 1;
     } else if (computerChoice == "paper" && humanChoice == "scissors"){
-        alert("Player wins!")
+        console.log(`Player wins! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`)
+        humanScore += 1;
     } else if (computerChoice == "scissors" && humanChoice == "rock"){
-        alert("Player wins!")
+        console.log(`Player wins! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`)
+        humanScore += 1;
     } else {
-        alert("Nooo! Player lost!")
+        console.log(`Nooo Player lost! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`)
+        computerScore += 1;
     }
 }
